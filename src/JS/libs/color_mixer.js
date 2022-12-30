c5c145eadc9c82e785c1a6121e22b8afd2be71e7
slowloris-coding <65670619+slowloris-coding@users.noreply.github.com>
2022-12-30 18:30:46 +0100

    Initial

---
+++ src/JS/libs/color_mixer.js
@@ -0,0 +1,59 @@
+/*!
+ * Color_mixer for substractive color mixing
+ * requires jQuery and jQuery color
+ * Author: Andy Soiron
+ * http://www.andysoiron.de
+ */
+
+Color_mixer = function(){};
+
+Color_mixer.toCymk = function(color){
+  cyan    = 255 - color._rgba[0];
+  magenta = 255 - color._rgba[1];
+  yellow  = 255 - color._rgba[2];
+  black   = Math.min(cyan, magenta, yellow);
+  cyan    = ((cyan - black) / (255 - black));
+  magenta = ((magenta - black) / (255 - black));
+  yellow  = ((yellow  - black) / (255 - black));
+
+  return {c:cyan,m:magenta,y:yellow,k:black/255,a:color._rgba[3]};
+}
+
+Color_mixer.toRgba = function(color){
+  color.c = color.c;
+  color.m = color.m;
+  color.y = color.y;
+  color.k = color.k;
+  R = color.c * (1.0 - color.k) + color.k;
+  G = color.m * (1.0 - color.k) + color.k;
+  B = color.y * (1.0 - color.k) + color.k;
+  R = Math.round((1.0 - R) * 255.0 + 0.5);
+  G = Math.round((1.0 - G) * 255.0 + 0.5);
+  B = Math.round((1.0 - B) * 255.0 + 0.5);
+  color = $.Color(R,G,B,color.a);
+  return color;
+}
+
+Color_mixer.mix = function(color_arr){
+  var color = {c:0,m:0,y:0,k:0,a:0};
+
+  color_arr.forEach((e, index) => {
+    color_arr[index] = Color_mixer.toCymk(e);
+    e = color_arr[index];
+    color.c += e.c;
+    color.m += e.m;
+    color.y += e.y;
+    color.k += e.k;
+    color.a += e.a;
+  })
+
+  color.c /= color_arr.length;
+  color.m /= color_arr.length;
+  color.y /= color_arr.length;
+  color.k /= color_arr.length;
+  color.a /= color_arr.length;
+
+  color = Color_mixer.toRgba(color);
+
+  return color;
+}
---
+++ src/JS/main.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+    const Colors = {
+        a: [255, 229, 0],  // *
+        b: [255, 153, 51],
+        c: [255, 255, 51],
+        d: [247, 170, 19],
+        e: [240, 125, 0],  // *
+        f: [51, 255, 153],
+        g: [51, 255, 255],
+        h: [249, 213, 6],
+        i: [228, 6, 19],   // *
+        j: [232, 78, 14],  // *
+        k: [255, 51, 255],
+        l: [111, 34, 130], // *
+        m: [0, 150, 63],   // *
+        n: [0, 122, 195],  // *
+        o: [1, 155, 165],  // *
+        p: [153, 221, 51],
+        q: [51, 221, 51],
+        r: [200, 5, 127],  // *
+        s: [0, 79, 158],   // *
+        t: [151, 193, 31], // *
+        u: [251, 187, 0],  // *
+        v: [153, 51, 221],
+        w: [221, 51, 221],
+        x: [221, 51, 153],
+        y: [187, 51, 51],
+        z: [187, 153, 51]
+    };
+
+    function mixRGB_evenly(color_arr) {
+        var r = 0;
+        var g = 0;
+        var b = 0;
+
+        color_arr.forEach((e) => {
+            r += e[0];
+            g += e[1];
+            b += e[2];
+        });
+
+        return [Math.ceil(r / color_arr.length), Math.ceil(g / color_arr.length), Math.ceil(b / color_arr.length)];
+    }
+
+    function rgbToHex(rgb) {
+        const r = rgb[0].toString(16).padStart(2, '0');
+        const g = rgb[1].toString(16).padStart(2, '0');
+        const b = rgb[2].toString(16).padStart(2, '0');
+
+        return `#${r}${g}${b}`;
+    }
+
+    function getStringColor(str) {
+        const color_arr = [];
+
+        for (let c of str) {
+            if (Colors.hasOwnProperty(c)) {
+                color_arr.push(Colors[c]);
+            }
+        }
+
+        const color_hex = rgbToHex(mixRGB_evenly(color_arr));
+
+        return color_hex;
+    }
+
+    $('#strInput').on('input', function() {
+        const strInputVal = $('#strInput').val();
+        const strColor = (strInputVal.length > 0 ? getStringColor(strInputVal.toLowerCase()) : '#ffffff');
+
+        $('#strColor').css('background-color', strColor);
+        $('#colorHexStr').text(strColor.toUpperCase());
+    })
+});
